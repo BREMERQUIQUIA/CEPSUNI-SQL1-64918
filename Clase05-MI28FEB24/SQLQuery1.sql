68bd40001490beec194d079474b9691fac187185
Eric Gustavo Coronel Castillo <gcoronel@vallegrande.edu.pe>
2024-02-28 19:41:01 -0500

    Clase 5

---
+++ Clase05-MI28FEB24/SQLQuery1.sql
@@ -0,0 +1,54 @@
+
+-- Sintaxis Basica
+/*
+SELECT [TOP <n> [WITH TIES]] {*|<LISTA_DE_COLUMNAS>}
+FROM [BD][ESQUEMA]<NOMBRE_TABLA>
+WHERE <FILTRO_FILAS>
+ORDER BY <COL_1>[ASC|DESC][,<COL_N>}
+*/
+
+
+-- Ejemplo 1
+-- Consultando el contenido de la tabla ALUMNO
+
+SELECT * FROM EDUCA2..ALUMNO;
+GO
+
+-- Ejemplo 2
+-- Consultando el ID y nombre de los alumnos.
+
+SELECT alu_id, alu_nombre
+FROM EDUCA2..ALUMNO;
+GO
+
+
+-- Ejemplo 3
+-- Consultando el ID y nombre de los alumnos.
+-- Ordenar el resultado por nombre.
+
+SELECT alu_id, alu_nombre
+FROM EDUCA2..ALUMNO
+ORDER BY alu_nombre;
+GO
+
+
+-- Ejemplo 4
+-- Consultando el ID y nombre de los alumnos.
+-- Ordenar el resultado por nombre en forma desc.
+-- Mostrar solo los 3 primeros registros.
+
+SELECT TOP 3 alu_id, alu_nombre
+FROM EDUCA2..ALUMNO
+ORDER BY alu_nombre DESC;
+GO
+
+-- Ejemplo 5
+-- Uso de expresiones
+
+SELECT C.*, 
+	(C.cur_vacantes - C.cur_matriculados) "Disponibles",
+	(cur_matriculados * cur_precio) "Ingreso estimado"
+FROM EDUCA..CURSO C;
+GO
+
+
